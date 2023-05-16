818e0f0a65b8c16c5d1fdf2406787132396d4d45
Gustavo André Richter <gustavoarichter@gmail.com>
2023-05-15 22:43:26 -0300

    Ajuste nos requires dos arquivos.

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/Controllers/AbstractController.php';
-require __DIR__ . '/Entity/Vaga.php';
-require __DIR__ . '/Db/Database.php';
-require __DIR__ . '/Routes/route.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/Controllers/AbstractController.php';
+require_once __DIR__ . '/Providers/AbstractService.php';
+require_once __DIR__ . '/Providers/ApiKeyMiddleware.php';
+require_once __DIR__ . '/Entity/Vaga.php';
+require_once __DIR__ . '/Db/Database.php';
+require_once __DIR__ . '/Routes/route.php';
