4a2cc066486dbe75939853689efb89d622956deb
Gustavo André Richter <gustavoarichter@gmail.com>
2023-05-15 22:45:08 -0300

    Adiciona middleware para autenticação da api.

--- src/Routes/route.php
+++ src/Routes/route.php
@@ -3,12 +3,16 @@
 namespace Src\Routes;
 
 use Src\Controllers\AbstractController;
+use Src\Providers\ApiKeyMiddleware;
 
 $app = new \Slim\App;
-$app->get('/getUsers', function ($request, $response, $args) {
-    $teste = new AbstractController();
-    $user = $teste->usuarios();
-    return $response->withJson($user, 200);
-});
+$app->group('/api', function () use ($app) {
+
+    $app->get('/getUsers', function ($request, $response, $args) {
+        $teste = new AbstractController();
+        $user = $teste->usuarios();
+        return $response->withJson($user, 200);
+    });
+})->add(new ApiKeyMiddleware());
 
 $app->run();
