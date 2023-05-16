088d329a773589c1f0ec49e036eba1b96b1c2dba
Gustavo André Richter <gustavoarichter@gmail.com>
2023-05-15 22:46:34 -0300

    Adiciona comentario de middleware da api.

--- src/Providers/ApiKeyMiddleware.php
+++ src/Providers/ApiKeyMiddleware.php
@@ -5,6 +5,13 @@ namespace Src\Providers;
 use Src\Providers\AbstractService;
 
 class ApiKeyMiddleware {
+    /**
+     * @param mixed $request
+     * @param mixed $response
+     * @param mixed $next
+     * Novo middleware que verifica a validade da x-api-key
+     * @return [type]
+     */
     public function __invoke($request, $response, $next) {
         $isValidApi = new AbstractService;
         $apiKey = $request->getHeaderLine('X-Api-Key');
