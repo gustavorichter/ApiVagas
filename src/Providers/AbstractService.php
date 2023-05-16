e5b9a31e40c620f70baad144a03cffdad03b7eab
Gustavo André Richter <gustavoarichter@gmail.com>
2023-05-15 22:44:00 -0300

    Nova função que verifica a validade da x-api-key.

--- src/Providers/AbstractService.php
+++ src/Providers/AbstractService.php
@@ -3,5 +3,15 @@
 namespace Src\Providers;
 
 class AbstractService {
-    
+    /**
+     * @param String $apiKey
+     *Valida se a chave da API é valida
+     * @return bool
+     */
+    function isValidApiKey( String $apiKey) : bool {
+        if ($apiKey == '862a2f7d-a9a4-488a-a3d8-299e9875df7es') {
+            return true;
+        }
+        return false;
+    }
 }
