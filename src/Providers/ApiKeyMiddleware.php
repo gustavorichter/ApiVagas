92ee95750bdc37cc2de67c9950284380885a8b7c
Gustavo André Richter <gustavoarichter@gmail.com>
2023-06-27 20:02:39 -0300

    chore: adiciona requisitos para cors

--- src/Providers/ApiKeyMiddleware.php
+++ src/Providers/ApiKeyMiddleware.php
@@ -20,6 +20,10 @@ class ApiKeyMiddleware {
             return $response->withJson($responseData, 401);
         }
         $response = $next($request, $response);
+        $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         return $response;
     }
 }
--- src/Routes/route.php
+++ src/Routes/route.php
@@ -64,8 +64,46 @@ class ApiRoutes {
                 }
             });
 
+            // $app->options('/{routes:.+}', function ($request, $response, $args) {
+            //     return $response;
+            // });
+
+            // $app->add(function ($req, $res, $next) {
+            //     $response = $next($req, $res);
+            //     return $response
+            //             ->withHeader('Access-Control-Allow-Origin', '*')
+            //             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            //             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+            // });
+
+            // // Catch-all route to serve a 404 Not Found page if none of the routes match
+            // // NOTE: make sure this route is defined last
+            // $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+            //     $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+            //     return $handler($req, $res);
+            // });
+
 		})->add(new ApiKeyMiddleware());
 
-		$app->run();
+        $corsMiddleware = function (Request $request, Response $response, $next) {
+            // Adiciona os cabeçalhos CORS
+            $response = $response
+                ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+                ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Api-Key');
+
+            // Verifica se é uma requisição OPTIONS e retorna a resposta imediatamente
+            if ($request->getMethod() === 'OPTIONS') {
+                return $response;
+            }
+
+            // Chama o próximo middleware
+            $response = $next($request, $response);
+
+            return $response;
+        };
+
+        $app->add($corsMiddleware);
+        $app->run();
 	}
 }
