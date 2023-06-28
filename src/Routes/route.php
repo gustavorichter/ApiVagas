c9dccd374ce4997ba5c268b424b7c0170b7afe38
Gustavo André Richter <gustavoarichter@gmail.com>
2023-06-27 21:56:09 -0300

    style: remoção de codigo não usado e formatação de código

--- src/Routes/route.php
+++ src/Routes/route.php
@@ -9,29 +9,29 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Slim\App;
 
 class ApiRoutes {
-	public function __construct() {
+    public function __construct() {
         $this->registerRoutes();
     }
 
-	public function registerRoutes() {
-		$app = new App;
+    public function registerRoutes() {
+        $app = new App;
 
-		$app->group('/api', function () use ($app) {
-			$app->get('/vagas', function (Request $request, Response $response, array $args) {
-				$objVaga = new AbstractController();
-				$user = $objVaga->vagas();
-				return $response->withJson($user, 200);
-			});
+        $app->group('/api', function () use ($app) {
+            $app->get('/vagas', function (Request $request, Response $response, array $args) {
+                $objVaga = new AbstractController();
+                $user = $objVaga->vagas();
+                return $response->withJson($user, 200);
+            });
 
-			$app->post('/novaVaga', function (Request $request, Response $response) {
-				$data = $request->getParsedBody();
-				$obj = new AbstractController();
+            $app->post('/novaVaga', function (Request $request, Response $response) {
+                $data = $request->getParsedBody();
+                $obj = new AbstractController();
 
-				$retorno = $obj->salvarVaga($data);
-				return $response->withJson($retorno, 200);
-			});
+                $retorno = $obj->salvarVaga($data);
+                return $response->withJson($retorno, 200);
+            });
 
-			$app->put('/vagas/{id}', function (Request $request, Response $response, array $args) {
+            $app->put('/vagas/{id}', function (Request $request, Response $response, array $args) {
                 $id = $args['id'];
                 $data = $request->getParsedBody();
                 $obj = new AbstractController();
@@ -52,7 +52,7 @@ class ApiRoutes {
                 }
             });
 
-			$app->delete('/vaga/{id}', function (Request $request, Response $response, array $args) {
+            $app->delete('/vaga/{id}', function (Request $request, Response $response, array $args) {
                 $id = $args['id'];
                 $obj = new AbstractController();
 
@@ -63,27 +63,7 @@ class ApiRoutes {
                     return $response->withStatus(404)->withJson(['message' => 'Vaga não encontrada']);
                 }
             });
-
-            // $app->options('/{routes:.+}', function ($request, $response, $args) {
-            //     return $response;
-            // });
-
-            // $app->add(function ($req, $res, $next) {
-            //     $response = $next($req, $res);
-            //     return $response
-            //             ->withHeader('Access-Control-Allow-Origin', '*')
-            //             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            //             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-            // });
-
-            // // Catch-all route to serve a 404 Not Found page if none of the routes match
-            // // NOTE: make sure this route is defined last
-            // $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
-            //     $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
-            //     return $handler($req, $res);
-            // });
-
-		})->add(new ApiKeyMiddleware());
+        })->add(new ApiKeyMiddleware());
 
         $corsMiddleware = function (Request $request, Response $response, $next) {
             // Adiciona os cabeçalhos CORS
@@ -92,7 +72,6 @@ class ApiRoutes {
                 ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
                 ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Api-Key');
 
-            // Verifica se é uma requisição OPTIONS e retorna a resposta imediatamente
             if ($request->getMethod() === 'OPTIONS') {
                 return $response;
             }
@@ -105,5 +84,5 @@ class ApiRoutes {
 
         $app->add($corsMiddleware);
         $app->run();
-	}
+    }
 }
