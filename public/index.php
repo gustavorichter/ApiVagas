0158f60ffc64ec3faf8e2239813e28798d281b7b
Gustavo André Richter <gustavoarichter@gmail.com>
2023-07-08 19:55:34 -0300

    chore: redireciona para o site quando não for feita a chamada da API.

--- public/index.php
+++ public/index.php
@@ -2,4 +2,10 @@
 require_once __DIR__ . '/../src/vendor/autoload.php';
 use Src\Routes\ApiRoutes;
 
-$apiRoutes = new ApiRoutes();
+$contentType = isset($_SERVER['CONTENT_TYPE']) ? $_SERVER['CONTENT_TYPE'] : '';
+
+if ($contentType === 'application/json') {
+    $apiRoutes = new ApiRoutes();
+} else {
+    echo 'site';
+}
